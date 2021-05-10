3a393b603c530973e20f3011dce59720ac3b52a7
Ravi Majithia <ravikiran.majithia@joubel.com>
2021-05-10 12:57:28 +0200

    Revert "Refactor firefox fullscreen height issue with IV"
    This reverts commit 73ee2fa116d4d1160897caa08adf9b08f4c98da1.

--- scripts/libraryScreen.js
+++ scripts/libraryScreen.js
@@ -1498,7 +1498,7 @@ H5P.BranchingScenario.LibraryScreen = (function () {
 
       // Preserve aspect ratio for Image in fullscreen (since height is limited) instead of scrolling or streching
       if (canScaleImage) {
-        const videoRect = (isVideo ? element.lastChild.getBoundingClientRect() : null); 
+        const videoRect = (isVideo ? element.lastChild.getBoundingClientRect() : null);
         if (videoRect || isHotspots || isCP || isImage) {
           const height = isHotspots ? instance.options.image.height : (isVideo ? videoRect.height : instance.height);
           const width = isHotspots ? instance.options.image.width : (isCP ? instance.ratio * height : (isVideo ? videoRect.width : instance.width));
@@ -1548,9 +1548,9 @@ H5P.BranchingScenario.LibraryScreen = (function () {
     }
 
     if (instance) {
-      this.handleLibraryResize();
       setTimeout(() => {
         instance.trigger('resize', e);
+        this.handleLibraryResize();
       }, 500);
     }
   };
