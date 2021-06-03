d0b911385071f547389e2f4730377d24548d61b8
Ravi Majithia <ravikiran.majithia@joubel.com>
2021-06-03 13:24:18 +0200

    Fix BS traversal order when going Back and fix video autoplay

--- scripts/libraryScreen.js
+++ scripts/libraryScreen.js
@@ -1275,10 +1275,18 @@ H5P.BranchingScenario.LibraryScreen = (function () {
    * Other libraries do not require reset
    */
   LibraryScreen.prototype.resetInstance = function () {
+    const autoPlay = (this.currentLibraryInstance.libraryInfo.machineName === "H5P.InteractiveVideo" || this.currentLibraryInstance.libraryInfo.machineName === "H5P.Video") 
+    ? this.shouldAutoplay[this.currentLibraryId]
+    : false;
+
     if (this.currentLibraryInstance.libraryInfo.machineName === "H5P.InteractiveVideo"
     && typeof this.currentLibraryInstance.resetTask !== "undefined") { 
       this.resetIVProgress();
       this.currentLibraryInstance.resetTask();
+      this.currentLibraryInstance.pause();
+      if (autoPlay && this.currentLibraryInstance.play !== undefined) {
+        this.currentLibraryInstance.play();
+      }
     }
     if (this.currentLibraryInstance.libraryInfo.machineName === "H5P.CoursePresentation" 
     && typeof this.currentLibraryInstance.resetTask !== "undefined") {
@@ -1287,6 +1295,10 @@ H5P.BranchingScenario.LibraryScreen = (function () {
     if (this.currentLibraryInstance.libraryInfo.machineName === "H5P.Video" 
     && typeof this.currentLibraryInstance.seek !== "undefined") {
       this.currentLibraryInstance.seek(0);
+      this.currentLibraryInstance.pause();
+      if (autoPlay && this.currentLibraryInstance.play !== undefined) {
+        this.currentLibraryInstance.play();
+      }
     }
   };
   /**
@@ -1342,6 +1354,11 @@ H5P.BranchingScenario.LibraryScreen = (function () {
           }
         });
         this.showBackgroundToReadspeaker();
+      }
+
+      // Even if it is comming from BQ, identify which BQ we are talking about
+      if (this.parent.getLibrary(this.currentLibraryId).contentId !== library.contentId) {
+        this.isBQReverse = false;
       }
 
       // Reset instance if BQ's Back button is pressed
