ac59dda06ab69392f991c5771fae8c8a55a4a448
Hannaes <hanna.solstad@joubel.com>
2021-06-06 18:34:15 +0200

    Fix BQ not using full height in fullscreen iphone
    We need to use the fullscreen function that also checks if it is semi-fullscreen

--- scripts/libraryScreen.js
+++ scripts/libraryScreen.js
@@ -1499,7 +1499,7 @@ H5P.BranchingScenario.LibraryScreen = (function () {
        * Resizes the wrapper to the height of the container. If the current BQ is at the very start of the content type then resize parent wrapper
        * Make exception for starting screen, so it does not cut from the top, as well as fullscreen.
        */
-      const isFullscreen = document.body.classList.contains('h5p-fullscreen');
+      const isFullscreen = this.parent.isFullScreen();
       const isSmallerDevice = this.parent.$container[0].classList.contains('h5p-mobile-screen');
 
       if (this.currentLibraryWrapper.style.height === "" && !this.parent.startScreen.isShowing && !isFullscreen && !isSmallerDevice) {
