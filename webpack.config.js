6117e89e343d011833c94d630093e54933ade7bc
devland <5208532+devland@users.noreply.github.com>
2024-02-02 12:35:53 +0100

    Hfp 3832 resume (#142)
    * HFP-3832 Modernize as part of refactoring
    
    - Remove unused/obsolete modules (eslint-plugin-react, @babel/eslint-parser, cross-env)
    - Update modules
    - Modernize webpack config and adjust library.json
    - Update eslint config and add `lint` script
    - Change file structure
    - Replace use of jQuery to vanilla JS unless required
    - Use ES6+ instead ES5
    
    * HFP-3832 Bump patch version
    
    - Required to apply changes in library.json for file location
    - Preparing to use H5P.Empty which requires H5P Core 1.26+
    
    * HFP-3832 Add support for resuming
    
    - Requires changes to H5P.BranchingQuestion to be in place
    
    * HFP-3832 Only store state of last node
    
    * HFP-3832 Add xAPI to previous state
    
    * HFP-3834 Fix restoring state
    
    ---------
    
    Co-authored-by: Oliver Tacke <o.tacke@posteo.de>

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,90 @@
-const path = require('path');
-const nodeEnv = process.env.NODE_ENV || 'development';
+import { dirname, resolve as _resolve, join } from 'path';
+import { fileURLToPath } from 'url';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserPlugin from 'terser-webpack-plugin'; // Provided by webpack
 
-module.exports = {
-  mode: nodeEnv,
-  context: path.resolve(__dirname, 'scripts'),
-  entry: {
-    entries: [
-      './branchingScenario.js',
-      './genericScreen.js',
-      './libraryScreen.js',
-      './libraryScreenOverlay.js',
-      './scoring.js'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const mode = process.argv.includes('--mode=production') ?
+  'production' :
+  'development';
+const libraryName = process.env.npm_package_name;
+
+export default {
+  mode: mode,
+  resolve: {
+    alias: {
+      '@assets': _resolve(__dirname, 'src/assets'),
+      '@scripts': _resolve(__dirname, 'src/scripts'),
+      '@services': _resolve(__dirname, 'src/scripts/services'),
+      '@styles': _resolve(__dirname, 'src/styles')
+    }
+  },
+  optimization: {
+    minimize: mode === 'production',
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          }
+        }
+      })
     ]
   },
-  devtool: (nodeEnv == 'development') ? 'eval-source-map' : false,
-  output: {
-    filename: 'dist.js',
-    path: path.resolve(__dirname, 'dist')
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${libraryName}.css`
+    })
+  ],
+  entry: {
+    dist: './src/entries/dist.js'
   },
+  output: {
+    filename: `${libraryName}.js`,
+    path: _resolve(__dirname, 'dist'),
+    clean: true
+  },
+  target: ['browserslist'],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        loader: 'babel-loader'
       },
+      {
+        test: /\.(s[ac]ss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg|\.jpg|\.png$/,
+        include: join(__dirname, 'src/assets'),
+        type: 'asset/resource'
+      },
+      {
+        test: /\.woff$/,
+        include: join(__dirname, 'src/fonts'),
+        type: 'asset/resource'
+      }
     ]
-  }
+  },
+  stats: {
+    colors: true
+  },
+  ...(mode !== 'production' && { devtool: 'eval-cheap-module-source-map' })
 };
